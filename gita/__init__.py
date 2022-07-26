e0f1a9342e684e03f7e6e4359830a4bbb78c6b7c
jerryw <a@casfdj.co>
2022-07-26 23:54:15 +0800

    ababa

--- gita/__init__.py
+++ gita/__init__.py
@@ -3,3 +3,4 @@ def hello(d):
     print('a%s'%d)
 if __name__  =='__main__':
  hello(3435345 )
+ print(2)
--- gitj/A.java
+++ gitj/A.java
@@ -2,6 +2,7 @@ public class A{
     public static void main(String[] args){
         int  __a_test
 
+
         = 2;;;
     }
 }
